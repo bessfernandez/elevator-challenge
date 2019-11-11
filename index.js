60c2cca749e1fd95cf77c6b6b1a8a107e74a7032
Bess <bessfernandez@gmail.com>
2019-11-11 15:00:18 -0800

    Adding basic state class, requirements

--- index.js
+++ index.js
@@ -1,4 +1,29 @@
 
+// ElevatorState manages all state updates between classes
+class ElevatorState {
+  constructor(elevators) {
+    if (!elevators) {
+      throw new Error('Must provide Elevators in order to track their state')
+    }
+
+    this.allElevators = elevators;
+  }
+  moveElevator() {
+    // Each elevator will report as is moves from floor to floor.
+    
+
+    // An elevator request can be made at any floor, to go to any other floor.
+
+
+  }
+  reportMovement() {
+    // Each elevator will report when it opens or closes its doors.
+  }
+  reportDoorState() {
+
+  }
+}
+
 class Elevator {
   constructor() {
     // initial elevator assumptions
@@ -24,10 +49,26 @@ class ElevatorSimulator {
       this.allElevators.push(new Elevator())
     }
 
-    console.log(this.allElevators, floors)
+    this.elevatorState = new ElevatorState(this.allElevators);
+  }
+  requestElevatorMovement(elevator, floorRequested = 1) {
+    // An elevator cannot proceed above the top floor.
+    if (floorRequested > this.totalFloors) {
+      throw new Error('Cannot move elevator, that floor exceeds total floors')
+    }
+    
+    // An elevator cannot proceed below the ground floor (assume 1 as the min)
+    if (floorRequested < 1) {
+      throw new Error('Cannot move elevator below first floor')
+    }
 
+    
+    console.log(this.elevatorState)
   }
 }
   
 // create elevator
-const myElevators = new ElevatorSimulator(2, 4)
+const myElevatorSimulator = new ElevatorSimulator(2, 4);
+
+// request specific elevator ro move to a floor
+myElevatorSimulator.requestElevatorMovement(1, 3);
